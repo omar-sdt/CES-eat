d25c09a72601eb63e9160afc5e36f36c216a60d7
Noémie <50912955+koyppa@users.noreply.github.com>
2025-04-04 09:20:15 +0200

    Création du form profil sans les boutons

---
+++ frontend/src/components/ui/my-profile-form.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { useAuth } from "@/context/auth-context"
+import { useState } from "react"
+
+export function MyProfileForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const { logout } = useAuth()
+
+  const [firstName, setFirstName] = useState("Liberty")
+  const [lastName, setLastName] = useState("Sabatier")
+  const [phone, setPhone] = useState("06.12.34.56.78")
+  const [email, setEmail] = useState("liberty.croquettes@meow.com")
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader className="text-center">
+          <CardTitle className="text-2xl">Mon Profil</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-6">
+          <div className="grid gap-2">
+            <Label htmlFor="firstName">Prénom</Label>
+            <Input
+              id="firstName"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="lastName">Nom</Label>
+            <Input
+              id="lastName"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="phone">Numéro de téléphone</Label>
+            <Input
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Adresse mail</Label>
+            <Input
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,7 +1,14 @@
+import { MyProfileForm } from "@/components/ui/my-profile-form"
+
 const Profile = () => {
     return (
         <>
-            <div>Mon compte</div>
+
+                    <div className="flex w-full justify-center">
+                        <div className="w-full max-w-3xl mt-4">
+                            <MyProfileForm />
+                        </div>
+                    </div>
         </>
     )
 }
