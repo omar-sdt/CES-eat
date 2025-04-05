01154594c9a40511e43d6226be82b5c21b71cd74
mohaCESI cesi <mohamed.himeur@viacesi.fr>
2025-04-05 19:15:34 +0000

    adding design of the command object

--- frontend/src/components/ui/order-card.tsx
+++ frontend/src/components/ui/order-card.tsx
@@ -22,7 +22,32 @@ const OrderCard = ({
   deliveryTime,
 }: OrderProps) => {
   return (
-    
+    <div className="flex flex-col border-2 rounded-sm p-4 gap-2">
+      <h2 className="text-lg font-semibold">Commande #{id}</h2>
+      <p className="text-sm">
+        <span className="font-semibold">Client :</span> {clientName}
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Commande :</span> {items.join(", ")}
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Date :</span>{" "}
+        {format(new Date(orderDate), "dd/MM/yyyy")}
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Heure de livraison :</span>{" "}
+        {format(new Date(deliveryTime), "HH:mm")}
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Prix :</span> {totalPrice.toFixed(2)} €
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Type :</span> {orderType}
+      </p>
+      <p className="text-sm">
+        <span className="font-semibold">Statut :</span> {status}
+      </p>
+    </div>
   );
 };
 
